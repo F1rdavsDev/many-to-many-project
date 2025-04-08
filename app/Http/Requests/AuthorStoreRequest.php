dd95886e743c25b886e2889a9eb6dc56e1ff736e
F1rdavs9644 <begmatovsamandar726@gmail.com>
2025-04-08 15:22:24 +0500

    validation is written for Book and Author

--- app/Http/Requests/AuthorStoreRequest.php
+++ app/Http/Requests/AuthorStoreRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AuthorCreateRequest extends FormRequest
+class AuthorStoreRequest extends FormRequest
 {
     
     /**
